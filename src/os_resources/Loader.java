ea5bdc14f4b31177a4541e368bcdbf70bbee6b69
ccamp113 <ccamp113@students.kennesaw.edu>
2016-03-07 12:59:04 -0500

    Update Loader.java

--- src/os_resources/Loader.java
+++ src/os_resources/Loader.java
@@ -1,5 +1,19 @@
 package os_resources;
+import java.io.*;
+public class Loader 
+     {
+         public static void main (String[] args) throws IOException
+         {
+             String line;
+             BufferedReader in;
 
-public class Loader {
+             in = new BufferedReader(new FileReader("Program-File.txt"));
+             line = in.readLine();
 
-}
+             while(line != null)
+             {
+                   
+             }
+         }
+
+    }
