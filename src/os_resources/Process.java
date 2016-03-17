175bee07ddfbc7bc46e7dd42953b5023489cb02f
BruceHoudini <westonford@gmail.com>
2016-03-16 23:15:32 -0400

    Updates for class Process()

--- src/os_resources/Process.java
+++ src/os_resources/Process.java
@@ -1,8 +1,9 @@
 package os_resources;
 
 public class Process {
-	private int PID, pAddr, numInst, numData, priority, sizeInBuff, sizeOutBuff, sizeTempBuff;
 	
+	private int PID, pAddr, numInst, numData, priority, sizeInBuff, sizeOutBuff, sizeTempBuff, rAddrBegin, rAddrEnd;
+	private boolean loadedRAM, isReady;
 	public Process(int PID, int pAddr, int numInst, int numData, int priority, int sizeInBuff, int sizeOutBuff, int sizeTempBuff){
 		this.PID = PID;
 		this.pAddr = pAddr;
@@ -12,7 +13,14 @@ public class Process {
 		this.sizeInBuff = sizeInBuff;
 		this.sizeOutBuff = sizeOutBuff;
 		this.sizeTempBuff = sizeTempBuff;
+		rAddrBegin = 0;
+		rAddrEnd = 0;
+		loadedRAM = false;
+		isReady = false;
 	}
+	
+	//Obnoxiously long list of getters/setters
+	//May be entirely superfluous
 	
 	public int getPID(){
 		return PID;
@@ -38,6 +46,60 @@ public class Process {
 	public int getSizeTempBuff(){
 		return sizeTempBuff;
 	}
+	public int getRAddrBegin(){
+		return rAddrBegin;
+	}
+	public int getRAddrEnd(){
+		return rAddrEnd;
+	}
+	public boolean isLoadedRAM(){
+		return loadedRAM;
+	}
+	public boolean readyStatus(){
+		return isReady;
+	}
+	public void setPID(int x){
+		PID = x;
+	}
+	public void setPAddr(int x){
+		pAddr = x;
+	}
+	public void setNumInst(int x){
+		numInst = x;
+	}
+	public void setNumData(int x){
+		numData = x;
+	}
+	public void setPriority(int x){
+		priority = x;
+	}
+	public void setSizeInBuff(int x){
+		sizeInBuff = x;
+	}
+	public void setSizeOutBuff(int x){
+		sizeOutBuff = x;
+	}
+	public void setSizeTempBuff(int x){
+		sizeTempBuff = x;
+	}
+	public void setRAddrBegin(int x){
+		rAddrBegin = x;
+	}
+	public void setRAddrEnd(int x){
+		rAddrEnd = x;
+	}
+	public void isLoaded(){
+		loadedRAM = true;
+	}
+	public void isNotLoaded(){
+		loadedRAM = false;
+	}
+	public void Ready(){
+		isReady = true;
+	}
+	public void notReady(){
+		isReady = false;
+	}
+	
 }
-
 
